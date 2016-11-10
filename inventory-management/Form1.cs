98e4e73f410f3886d8ad8fdbea242cbe22924845
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-10 17:28:39 +0000

    open dialog windows

--- inventory-management/Form1.cs
+++ inventory-management/Form1.cs
@@ -16,5 +16,23 @@ namespace inventory_management
         {
             InitializeComponent();
         }
+
+        private void issueGoodsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            issue_goods_form ig = new issue_goods_form();
+            ig.ShowDialog();
+        }
+
+        private void createProductToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            create_product_form cp = new create_product_form();
+            cp.ShowDialog();
+        }
+
+        private void editProductToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            edit_product_form ep = new edit_product_form();
+            ep.ShowDialog();
+        }
     }
 }
