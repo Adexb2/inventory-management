6176f8e357c28d502559816ad6312ebda0bfabbe
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-10 18:05:23 +0000

    format datagridview

--- inventory-management/Form1.Designer.cs
+++ inventory-management/Form1.Designer.cs
@@ -80,10 +80,14 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(769, 524);
             this.dataGridView1.TabIndex = 1;
             // 
